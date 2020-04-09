ff7d98937856d29a7a5fea9590bb42d1789671e8
Daniel Rivas <ers.daniel+dev@gmail.com>
2020-04-09 14:05:13 -0500

    Reduced sentence length in ride description

--- reports/ride_reviews.read.sql
+++ reports/ride_reviews.read.sql
@@ -1,4 +1,4 @@
-SELECT rides.id, rides.name, COUNT(reviews.*), AVG(reviews.rating)
+SELECT rides.id, rides.name, COUNT(reviews.*) AS review_count, AVG(reviews.rating)  AS review_avg
     FROM theme_park.rides
     LEFT JOIN theme_park.reviews ON reviews.ride_id = rides.id
     GROUP BY rides.id
