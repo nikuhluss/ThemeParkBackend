eda7f54561f05365e3bf372f1c090e94b2adbbf7
Daniel Rivas <ers.daniel+dev@gmail.com>
2020-04-09 14:31:09 -0500

    Updated port for prest

--- reports/ride_reviews.read.sql
+++ reports/ride_reviews.read.sql
@@ -1,4 +1,4 @@
-SELECT rides.id, rides.name, COUNT(reviews.*) AS review_count, AVG(reviews.rating)  AS review_avg
+SELECT rides.id AS ride_id, rides.name as ride_name, COUNT(reviews.*) AS review_count, AVG(reviews.rating)  AS review_avg
     FROM theme_park.rides
     LEFT JOIN theme_park.reviews ON reviews.ride_id = rides.id
     GROUP BY rides.id
