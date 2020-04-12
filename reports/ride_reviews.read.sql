ee7f50568dc8d6d5c19d2ed65c0b2bf278b152d6
Daniel Rivas <ers.daniel+dev@gmail.com>
2020-04-11 19:31:09 -0500

    Added ride sort

--- reports/ride_reviews.read.sql
+++ reports/ride_reviews.read.sql
@@ -1,4 +1,9 @@
-SELECT rides.id AS ride_id, rides.name as ride_name, COUNT(reviews.*) AS review_count, AVG(reviews.rating)  AS review_avg
-    FROM theme_park.rides
-    LEFT JOIN theme_park.reviews ON reviews.ride_id = rides.id
-    GROUP BY rides.id
+SELECT
+    rides.id AS ride_id,
+    rides.name as ride_name,
+    COUNT(reviews.*) AS review_count,
+    AVG(reviews.rating)  AS review_avg
+FROM theme_park.rides
+LEFT JOIN theme_park.reviews ON reviews.ride_id = rides.id
+GROUP BY rides.id
+ORDER BY ride_name ASC
